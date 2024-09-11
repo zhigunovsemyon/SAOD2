5a9ae07be05fdd665c93180ab003edd84e101cdb
Semyon <zhigunovsemen@gmail.com>
2024-09-11 15:26:20 +0300

    Сделал хендлер ввода

--- src/main.c
+++ src/main.c
@@ -8,8 +8,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Чтение ввода числа num из stdin по запросу из text */
+int InputHandler(int *num, const char *text) {
+    printf("%s", text);
+    int out = scanf("%d", num);
+    if (out == EOF) {
+        printf("\nВведён EOF. Завершение работы!\n");
+        return EOF;
+    } 
+    if (out < 1) {
+        printf("Некорректный ввод! Завершение работы!\n");
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
+
 int main(void) {
-	printf("Sample Text!\n");
+    int n;
+    if (InputHandler(&n, "Введите максимальное число на бочёнке: ")){
+        return EXIT_FAILURE;
+    }
+	printf("%d\n", n);
 
 	return EXIT_SUCCESS;
 }
